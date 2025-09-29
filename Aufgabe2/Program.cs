0eed04490fe1fab03b217bd8ed277a86c7e744d0
Dario691167 <dario.kuechlerblj@gmail.com>
2025-09-29 11:50:05 +0200

    Projektdateien hinzufügen.

---
+++ Aufgabe2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aufgabe2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bitte geben Sie eine Zahl ein.");
+            string anzTageInput = Console.ReadLine();
+            int anzTage = 0;
+
+            if (int.TryParse(anzTageInput, out anzTage) == true && anzTage > 27 && anzTage < 32)
+            {
+                Console.WriteLine("Ein Monat mit " + anzTage + "hat " + (anzTage * 86400) + " Sekunden");
+            }
+            else
+            {
+                Console.WriteLine("Eingabefehler. Bitte geben Sie eine ganze Zahl zwischen 28 und 31 ein.");
+            }
+        }
+    }
+}
---
+++ Aufgabengitbook/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aufgabe1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int Num1;
+            int Num2;
+            Console.WriteLine("Dieses Programm berechnet die Summe von 2 Zahlen");
+            Console.Write("Zahl 1: ");
+            Num1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Zahl 2: ");
+            Num2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(Num1 + Num2);
+
+        }
+    }
+}
